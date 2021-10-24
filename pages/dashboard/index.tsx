bbfa02a4e36780b2a3528998ffc3cca6fd9032c6
victor wick <victorwick@victors-MacBook-Air.local>
2021-10-24 18:59:06 +0700

    succes create login auth

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,7 +1,30 @@
+import { useSession } from "next-auth/client";
 export default function Index() {
-  return (
-    <div>
-      <p>This is the Dashboard page.</p>
-    </div>
-  );
+  const [session, loading] = useSession();
+
+  const isSessionValid = (session) => {
+    if (
+      typeof session !== typeof undefined &&
+      session !== null &&
+      typeof session.user !== typeof undefined
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  if (!loading) {
+    if (isSessionValid(session)) {
+      return <div className="wrapper">Welcome {session.user.email}</div>;
+    } else {
+      return (
+        <div className="wrapper">
+          <p>You are not logged in</p>
+        </div>
+      );
+    }
+  } else {
+    return null;
+  }
 }
