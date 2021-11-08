860b88e767035aa7494120ae6dfb77a99b0be2e4
resbayuaji <resbayuaji@gmail.com>
2021-11-08 19:38:36 +0700

    feat: add loading state while render dom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,41 @@
 import Head from "next/head";
 import { Provider } from "next-auth/client";
+import Router from "next/router";
 import "../styles/globals.css";
+import { useState } from "react";
+import { Spin } from "antd";
 
 function MyApp({ Component, pageProps }) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  Router.events.on("routeChangeStart", () => {
+    setIsLoading(true);
+  });
+
+  Router.events.on("routeChangeComplete", () => {
+    setIsLoading(false);
+  });
   return (
-    <Provider options={{ clientMaxAge: 0 }} session={pageProps.session}>
-      <Head>
-        {typeof window === "undefined" && (
-          <style
-            id="holderStyle"
-            dangerouslySetInnerHTML={{
-              __html: `
-         *, *::before, *::after {
-           transition: none!important;
-         }
-         `,
-            }}
-          />
-        )}
-      </Head>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Provider options={{ clientMaxAge: 0 }} session={pageProps.session}>
+        <Head>
+          {typeof window === "undefined" && (
+            <style
+              id="holderStyle"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  *, *::before, *::after {
+                    transition: none!important;
+                  }
+                  `,
+              }}
+            />
+          )}
+        </Head>
+        {isLoading && <Spin />}
+        {!isLoading && <Component {...pageProps} />}
+      </Provider>
+    </>
   );
 }
 
